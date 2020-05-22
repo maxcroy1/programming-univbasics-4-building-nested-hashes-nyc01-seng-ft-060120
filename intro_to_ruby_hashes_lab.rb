f9830678b3c1fa2b973888319f18e907384e3620
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 19:40:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base_hash = {railroads:{}}
-  base_hash[:railroads][:pieces] << 4
+  base_hash[:railroads] << {pieces: 4}
   base_hash[:railroads][:rent_in_dollars] = {}
   base_hash[:railroads][:names] = {}
   base_hash
